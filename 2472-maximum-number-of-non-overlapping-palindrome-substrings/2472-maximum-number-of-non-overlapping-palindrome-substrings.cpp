886611af102277e43db09e28db40a69b0a385aa6
Prateek Sharma <prateek1sharma.021@gmail.com>
2026-09-15 19:08:34 +0530

    Time: 1371 ms (8.37%), Space: 208.3 MB (17.28%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -11,26 +11,41 @@ public:
         }
         return true;
     }
-    int solve(string &s,int k,int i,int j,int n,vector<vector<int>>&dp){
-        if(i>=n || j>=n) return 0;
+    // int solve(string &s,int k,int i,int j,int n,vector<vector<int>>&dp){
+    //     if(i>=n || j>=n) return 0;
 
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(isPalin(s,i,j)){
-            int take=1+solve(s,k,j+1,j+k,n,dp);
-            int grow=solve(s,k,i,j+1,n,dp);
-            int slide=solve(s,k,i+1,j+1,n,dp);
-            return dp[i][j]=max(take,max(grow,slide));
-        }
-        else{
-            int grow=solve(s,k,i,j+1,n,dp);
-            int slide=solve(s,k,i+1,j+1,n,dp);
-            return dp[i][j]=max(grow,slide);
-        }
-    }
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //     if(isPalin(s,i,j)){
+    //         int take=1+solve(s,k,j+1,j+k,n,dp);
+    //         int grow=solve(s,k,i,j+1,n,dp);
+    //         int slide=solve(s,k,i+1,j+1,n,dp);
+    //         return dp[i][j]=max(take,max(grow,slide));
+    //     }
+    //     else{
+    //         int grow=solve(s,k,i,j+1,n,dp);
+    //         int slide=solve(s,k,i+1,j+1,n,dp);
+    //         return dp[i][j]=max(grow,slide);
+    //     }
+    // }
     int maxPalindromes(string s, int k) {
         int n=s.length();
         if(k==1) return n;
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        return solve(s,k,0,k-1,n,dp);
+        vector<vector<int>>dp(n+1,vector<int>(n+1));
+        // return solve(s,k,0,k-1,n,dp);
+
+        for(int i=n-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                if(isPalin(s,i,j)){
+                    int take=1+(j+k<=n?dp[j+1][j+k]:0);
+                    int grow=dp[i][j+1];
+                    int slide=dp[i+1][j+1];
+                    dp[i][j]=max(take,max(grow,slide));
+                }
+                int grow=dp[i][j+1];
+                int slide=dp[i+1][j+1];
+                dp[i][j]=max(dp[i][j],max(grow,slide));
+            }
+        }
+        return dp[0][k-1];
     }
 };
